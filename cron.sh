49221e0d7cb3010d600e3ee45b73d7d46dc82f28
Peter Schlömer (dapete) <dev@dapete.net>
2022-11-01 19:44:13 +0000

    Update to currently used configuration

--- cron.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#$ -N dewikinews-rss-update
-#$ -o /data/project/dewikinews-rss/update.log
-#$ -e /data/project/dewikinews-rss/update.log
-#$ -l h_vmem=500M,release=stretch
-
-php /data/project/dewikinews-rss/php/dewikinews-update.php
